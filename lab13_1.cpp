dc68b9ac4229660d90dd88428984ea946ba8addf
neokarn <34811101+neokarn@users.noreply.github.com>
2024-12-24 21:35:11 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 using namespace std;
 
@@ -9,6 +10,7 @@ int main(){
     int N = sizeof(A)/sizeof(A[0]);
     double B[6];
     stat(A,N,B);
+    cout << fixed << setprecision(2);
     cout << "Arithmetic Mean = " << B[0] << endl;
     cout << "Standard Deviation = " << B[1] << endl;
     cout << "Geometric Mean = " << B[2] << endl;
